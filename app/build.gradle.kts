e834e2104bacddbf40e6ba5bec58f73591390137
alfattahfinance <mhmdaril707@gmail.com>
2026-08-29 17:34:01 +0700

    feat: add native Android wrapper and media intents

---
+++ app/build.gradle.kts
@@ -0,0 +1,4 @@
+plugins { id("com.android.application") }
+android { namespace="com.alfattahfinance.mixeronline"; compileSdk=35
+    defaultConfig { applicationId="com.alfattahfinance.mixeronline"; minSdk=23; targetSdk=35; versionCode=1; versionName="1.0" }
+}
