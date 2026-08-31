ce634ab7f53c8f132e567824ed900c242d9db00e
alfattahfinance <mhmdaril707@gmail.com>
2026-08-31 22:51:35 +0700

    reset: remove existing Mixer-Online code

--- app/build.gradle.kts
+++
@@ -1,4 +0,0 @@
-plugins { id("com.android.application") }
-android { namespace="com.alfattahfinance.mixeronline"; compileSdk=35
-    defaultConfig { applicationId="com.alfattahfinance.mixeronline"; minSdk=23; targetSdk=35; versionCode=1; versionName="1.0" }
-}
